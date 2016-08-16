09772bd98231d636ffb07a34d268d35ea28bcbb6
Gabriel Correia <gmfc@users.noreply.github.com>
2016-08-16 08:50:19 -0300

    Update rlcg.js

--- rlcg.js
+++ rlcg.js
@@ -6,10 +6,10 @@ function rlcg(seed){
   } else {
     this.seed = bigInt(seed);
   }
-  this.A = bigInt("6364136223846793005");
-  this.C = bigInt("1442695040888963407");
-  this.M = bigInt("9223372036854775808");
-  this.IN = bigInt("13877824140714322085");
+  this.A = bigInt("1bk7am76uy5hcxq16fcqkwxmr",35);//6364136223846793005 //41c9roc33ffeys2wpfeta3wd
+  this.C = bigInt("1atak74jd74hd83756kncx458",35);//1442695040888963407//345245243524351213
+  this.M = bigInt("zzzzzzzzzzzzzzzzzzzzzzzzz",35);//9223372036854775808//zzzzzzzzzzzzzzzzzzzzzzzzz
+  this.IN = bigInt("41c9roc33ffeys2wpfeta3wd",35);//13877824140714322085//4654452103859546277//41c9roc33ffeys2wpfeta3wd
   
 }
 
@@ -20,6 +20,9 @@ rlcg.prototype.next = function(){
 
 rlcg.prototype.prev = function(){
   this.seed = bigInt(this.seed).minus(this.C).multiply(this.IN).mod(this.M);
+  if (this.seed.isNegative()) {
+    this.seed= bigInt(this.seed).plus(this.M);
+  }
   return(this.seed);
 };
 
